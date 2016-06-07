616a8634cac00c055308eb8a7c604c5d62487985
Elena <elena.ruiz@est.fib.upc.edu>
2016-06-07 20:16:59 +0200

    Quitando notas PAR xd

--- src/main/java/HibernateUtils.java
+++ src/main/java/HibernateUtils.java
@@ -1,7 +1,6 @@
 import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-
 import org.hibernate.cfg.Configuration;
 import org.hibernate.service.ServiceRegistry;
 import org.hibernate.service.ServiceRegistryBuilder;
