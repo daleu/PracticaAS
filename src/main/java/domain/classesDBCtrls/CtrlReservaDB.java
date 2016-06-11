13db7e6d50308a828602fc4ec501680c0a35ab67
cjf93 <cristian.prat.93@gmail.com>
2016-06-11 04:57:25 +0200

    Creada clase que implementa la interficie CtrlReserva(no implementada)

---
+++ src/main/java/domain/classesDBCtrls/CtrlReservaDB.java
@@ -0,0 +1,25 @@
+package domain.classesDBCtrls;
+
+import domain.classes.Reserva;
+import domain.controllers.CtrlReserva;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+/**
+ * Created by crist_000 on 11/06/2016.
+ */
+public class CtrlReservaDB implements CtrlReserva {
+    public Reserva getReserva(String nomRecurs, Integer horainici, Date data) {
+       // Reserva reserva =
+        return null;
+    }
+
+    public void insertarReserva(Reserva reserva) {
+
+    }
+
+    public ArrayList<Reserva> getAll() {
+        return null;
+    }
+}
