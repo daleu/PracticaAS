8e3fc927b371c53eccaf64f64dada25b695f95d5
David Aleu Mosegui <david.aleu@est.fib.upc.edu>
2016-06-11 17:24:22 +0200

    Afegida operacio get reserva i excepcio

--- src/main/java/domain/classesDBCtrls/CtrlReservaDB.java
+++ src/main/java/domain/classesDBCtrls/CtrlReservaDB.java
@@ -1,7 +1,13 @@
 package domain.classesDBCtrls;
 
 import domain.classes.Reserva;
+import domain.classes.ReservaAmbNotificacio;
+import domain.classes.ReservaPK;
 import domain.controllers.CtrlReserva;
+import domain.exceptions.NoHiHaReserva;
+import domain.hibernate.HibernateUtils;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 
 import java.util.ArrayList;
 import java.util.Date;
@@ -10,9 +16,16 @@ import java.util.Date;
  * Created by crist_000 on 11/06/2016.
  */
 public class CtrlReservaDB implements CtrlReserva {
-    public Reserva getReserva(String nomRecurs, Integer horainici, Date data) {
-       // Reserva reserva =
-        return null;
+    public Reserva getReserva(String nomRecurs, Integer horainici, Date data) throws NoHiHaReserva{
+
+        SessionFactory sf = HibernateUtils.getSessionFactory();
+        Session session = sf.openSession();
+
+        Reserva res = (ReservaAmbNotificacio) session.get(ReservaAmbNotificacio.class, ReservaAmbNotificacio.class);
+        session.close();
+        if(res == null)
+            throw new NoHiHaReserva();
+        return res;
     }
 
     public void insertarReserva(Reserva reserva) {
--- src/main/java/domain/controllers/CtrlReserva.java
+++ src/main/java/domain/controllers/CtrlReserva.java
@@ -1,6 +1,7 @@
 package domain.controllers;
 
 import domain.classes.Reserva;
+import domain.exceptions.NoHiHaReserva;
 
 import java.util.ArrayList;
 import java.util.Date;
@@ -9,7 +10,7 @@ import java.util.Date;
  * Created by crist_000 on 11/06/2016.
  */
 public interface CtrlReserva {
-    public Reserva getReserva(String nomRecurs, Integer horainici, Date data);
+    public Reserva getReserva(String nomRecurs, Integer horainici, Date data) throws NoHiHaReserva;
     public void insertarReserva(Reserva reserva);
     public ArrayList<Reserva> getAll();
 }
