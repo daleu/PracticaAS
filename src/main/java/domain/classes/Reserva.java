86fea8deeb1a762a508fbdc363f03bdd35457102
cjf93 <cristian.prat.93@gmail.com>
2016-06-11 16:15:06 +0200

    Cambiat Integer

--- src/main/java/domain/classes/Reserva.java
+++ src/main/java/domain/classes/Reserva.java
@@ -11,12 +11,13 @@ import java.sql.Time;
  */
 @Entity
 @Table(name = "reserva", schema = "public", catalog = "postgres")
-@Check(constraints = "(horainici < horafi) AND horaInici <= '23:59:59' AND horaFi>= '00:00:00' AND  horaFi<='23:59:59' AND horaInici >='00:00:00'")
+//@Check(constraints = "(horainici < horafi) AND horaInici <= '23:59:59' AND horaFi>= '00:00:00' AND  horaFi<='23:59:59' AND horaInici >='00:00:00'")
+@Check(constraints = "(horainici < horafi) AND horaInici <= '23' AND horaFi>= '1' AND  horaFi<='24' AND horaInici >='0'")
 @IdClass(ReservaPK.class)
 public class Reserva {
     private Date data;
-    private Time horainici;
-    private Time horafi;
+    private Integer horainici;
+    private Integer horafi;
     private String comentaris;
     private String nomrecurs;
     private String username;
@@ -26,7 +27,7 @@ public class Reserva {
     public Reserva() {
     }
 
-    public Reserva(Date data, Time horainici, Time horafi, String comentaris, String nomrecurs, String username) {
+    public Reserva(Date data, Integer horainici, Integer horafi, String comentaris, String nomrecurs, String username) {
         this.data = data;
         this.horainici = horainici;
         this.horafi = horafi;
@@ -48,21 +49,21 @@ public class Reserva {
 
     @Id
     @Column(name = "horainici", nullable = false)
-    public Time getHorainici() {
+    public Integer getHorainici() {
         return horainici;
     }
 
-    public void setHorainici(Time horainici) {
+    public void setHorainici(Integer horainici) {
         this.horainici = horainici;
     }
 
     @Basic
     @Column(name = "horafi", nullable = true)
-    public Time getHorafi() {
+    public Integer getHorafi() {
         return horafi;
     }
 
-    public void setHorafi(Time horafi) {
+    public void setHorafi(Integer horafi) {
         this.horafi = horafi;
     }
 
