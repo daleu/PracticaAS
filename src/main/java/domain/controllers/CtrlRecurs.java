b7a3660123d061bb53bb65ecbff3098c4e255116
cjf93 <cristian.prat.93@gmail.com>
2016-06-11 04:59:57 +0200

    Cambiado nombre de las funciones

--- src/main/java/domain/controllers/CtrlRecurs.java
+++ src/main/java/domain/controllers/CtrlRecurs.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
  * Created by crist_000 on 11/06/2016.
  */
 public interface CtrlRecurs {
-    public Recurs getLocal(String nomRecurs);
-    public void insertarLocal(Recurs recurs);
+    public Recurs getRecurs(String nomRecurs);
+    public void insertarRecurs(Recurs recurs);
     public ArrayList<Recurs> getAll();
 }
